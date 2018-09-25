b90114485c93baf86a356c0e241a48edd72f33e3
Frederic Boudon <frederic.boudon@cirad.fr>
2018-09-25 16:38:43 +0200

    improve html code

--- communication.py
+++ communication.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from flask import request
-from flask import render_template
+from flask import request, render_template, url_for, redirect
 from flask import Markup
 	
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return redirect(url_for('index_html'))
 
-@app.route('/interpret/<name>', methods=['GET', 'POST'])
+@app.route('/index.html')
+def index_html():
+    #return  file('index.html').read()
+    return render_template('result.html', name='', interpreter = url_for("interpret"), code = 'Axiom:\n\nderivation length: 1\nproduction:\n\ninterpretation:\n\nendlsystem\n'  )
+
+@app.route('/interpret.html', methods=['GET', 'POST'])
 def interpret(name=None):
     if request.method == 'POST':
 		import openalea.lpy as lpy
@@ -23,6 +27,6 @@ def interpret(name=None):
 		lstring = l.derive()
 		ilstring = l.interpret(lstring)
 		txtlstring = str(ilstring)
-		return render_template('result.html', name=txtlstring)
+		return render_template('result.html', name=txtlstring, interpreter = url_for("interpret"), code = code  )
     else:
         return 'error then return code'
